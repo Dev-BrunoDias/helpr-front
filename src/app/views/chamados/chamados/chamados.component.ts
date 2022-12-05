b9515e42eecc95196a51a97eb9e848fad0f74dc5
BrunoDiasLS <brunodlopes82@globo.com>
2022-12-05 11:49:41 -0300

    Crud chamados

---
+++ src/app/views/chamados/chamados/chamados.component.ts
@@ -0,0 +1,28 @@
+import { ChamadoService } from './../../../services/chamado.service';
+import { Component, OnInit } from '@angular/core';
+import { Chamado } from 'src/app/models/chamado';
+
+
+@Component({
+  selector: 'app-chamados',
+  templateUrl: './chamados.component.html',
+  styleUrls: ['./chamados.component.css']
+})
+export class ChamadosComponent implements OnInit {
+
+  displayedColumns: string[] = ['id', 'titulo', 'cliente', 'funcionario', 'dataAbertura', 'status', 'editar', 'detalhes'];
+  dataSource: Chamado[] = [];
+
+  constructor(private chamadoService: ChamadoService) { }
+
+  ngOnInit(): void {
+    this.initializeTable()
+  }
+
+  private initializeTable(): void {
+    this.chamadoService.findAll().subscribe(chamados => {
+      this.dataSource = chamados;
+    });
+  }
+
+}
